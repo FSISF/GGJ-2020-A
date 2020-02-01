5bd0aa9b6859c44976d3c7a4ecaf0e24a07da2e9
morikirin <kirin159361@gmail.com>
2020-02-01 18:49:43 +0800

    隨機性和鏡頭偵測

--- Assets/Script/Game/Enemy.cs
+++ Assets/Script/Game/Enemy.cs
@@ -15,11 +15,13 @@ public class Enemy : MonoBehaviour
     private float damageTime;           //累積正在破壞時間
     public float damageCompleteTime;    //破壞完成時間
     public int damagedLevel;            //破壞程度階級
-    private Vector2 originalEnemyPosition;
-    private float idleTime;
-    private int idleMaxTime;
-    public int speed;
+    private Vector2 originalEnemyPosition;  //開始遊戲時的位置
+    private float idleTime;             //閒置時間累計
+    private int idleMaxTime;            //最大閒置時間   
+    public int speed;                   //移動速度
+    private float lastTime = 0;
     private int derect = 1;
+    public Camera cam;
 
     void Start()
     {
@@ -49,11 +51,15 @@ public class Enemy : MonoBehaviour
 
     void Enemy_idle()
     {
-        transform.Translate(Vector2.right * Time.deltaTime * speed * derect);
-        if (transform.position.x >= originalEnemyPosition.x + 4)
-            derect = -1;
-        else if (transform.position.x <= originalEnemyPosition.x - 4)
-            derect = 1;
+        if (idleTime - lastTime >= 3 || idleTime == 0)
+        {
+            lastTime = idleTime;
+            int p = Random.Range(0, 99);
+            if (p <= 40)
+                transform.Translate(Vector2.right * Time.deltaTime * speed);
+            else if (40 < p && p <= 80)
+                transform.Translate(Vector2.right * Time.deltaTime * speed);
+        }
         if (idleTime == 0)
         {
             idleMaxTime = Random.Range(5, 10);
@@ -62,8 +68,17 @@ public class Enemy : MonoBehaviour
         Debug.Log(idleTime);
         if (idleTime >= idleMaxTime)
         {
-            enemyStateNow = enemyState.damage;
             idleTime = 0;
+            int i = Random.Range(0, 1);
+            switch (i)
+            {
+                case 0:
+                    enemyStateNow = enemyState.damage;
+                    break;
+                case 1:
+                    enemyStateNow = enemyState.cover;
+                    break;
+            }
         }
     }
 
@@ -82,6 +97,10 @@ public class Enemy : MonoBehaviour
                 break;
         }
         Debug.Log(damageTime);
+        if (Vector2.Distance(transform.position, cam.transform.position) <= Screen.width / 2)
+        {
+            enemyStateNow = enemyState.pretend;
+        }
         if (damageTime >= damageCompleteTime)
         {
             damagedLevel += 1;
@@ -96,7 +115,11 @@ public class Enemy : MonoBehaviour
     }
     void Enemy_pretend()
     {
-
+        if (Vector2.Distance(transform.position, cam.transform.position) > Screen.width / 2)
+        {
+            enemyStateNow = enemyState.damage;
+        }
+        transform.position = originalEnemyPosition;
         Debug.Log("吹口哨動畫");
     }
 }
