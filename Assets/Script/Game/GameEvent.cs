08ef7d3a6de3973f32e01c5434a400ecea8dfe2e
FSISF <a1455785@gmail.com>
2020-02-01 16:18:32 +0800

    遊戲事件

---
+++ Assets/Script/Game/GameEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void CheckOjectLevel();
+
+public class GameEvent
+{
+    public static event CheckOjectLevel CheckObjectLevel;
+    public static void DoCheckObjectLevel()
+    {
+        CheckObjectLevel();
+    }
+}
