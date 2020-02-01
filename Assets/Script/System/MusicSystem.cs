b8f05d1e3d01857664cbfd7ce66a1b5d5366ba3a
FSISF <a1455785@gmail.com>
2020-02-01 15:02:55 +0800

    音樂系統

---
+++ Assets/Script/System/MusicSystem.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BGMName
+{
+    Game,
+}
+
+public enum SoundName
+{
+}
+
+public class MusicSystem : SingletonMono<MusicSystem>
+{
+    public AudioSource BGM = null;
+    public AudioSource Sound = null;
+
+    private void Awake()
+    {
+        if (BGM == null)
+        {
+            BGM = this.gameObject.AddComponent<AudioSource>();
+        }
+        if (Sound == null)
+        {
+            Sound = this.gameObject.AddComponent<AudioSource>();
+        }
+    }
+
+    void Start()
+    {
+
+    }
+
+    public void PlayBGM(BGMName bGMName, bool loop = true)
+    {
+        AudioClip bgm = Resources.Load<AudioClip>("Music/BGM/" + bGMName.ToString());
+        if (bgm != null)
+        {
+            BGM.clip = bgm;
+            BGM.loop = loop;
+            BGM.Play();
+        }
+    }
+
+    public void PlaySound(SoundName soundName)
+    {
+        AudioClip sound = Resources.Load<AudioClip>("Music/Sound/" + soundName.ToString());
+        if (sound != null)
+        {
+            Sound.PlayOneShot(sound);
+        }
+    }
+}
