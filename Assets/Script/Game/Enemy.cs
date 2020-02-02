bbafb635302bda0ce42b4881719b5d39dec77a50
ani98741 <ani98741@gmail.com>
2020-02-02 09:49:07 +0800

    Revert "玩家站到 cover state 的犯人牢房後犯人回到 idle 狀態"
    This reverts commit c86ebf6d3323c781dbb8146d42f971e66cef633b.

--- Assets/Script/Game/Enemy.cs
+++ Assets/Script/Game/Enemy.cs
@@ -127,10 +127,6 @@ public class Enemy : MonoBehaviour
         Debug.Log("假裝挖掘");
         coverTime += Time.deltaTime;
         if (coverTime >= 30) {
-            if (cam - number == 0)
-            {
-                enemyStateNow = enemyState.idle;
-            }
             if (Mathf.Abs(cam - number) >= 1)
                 Debug.Log("全房出現提示");
         }
